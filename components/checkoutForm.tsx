b63e3f7c70fbda82df7e0ffa2d4f2673697e4732
thunderhead27 <thunderhead27@gmail.com>
2023-04-24 06:50:32 -0700

    sixth commit

--- components/checkoutForm.tsx
+++ components/checkoutForm.tsx
@@ -64,7 +64,7 @@ export default function CheckoutForm() {
             elements,
             confirmParams: {
                 // Make sure to change this to your payment completion page
-                return_url: "/thankYou",
+                return_url: "https://pizza-tut.vercel.app/thankYou",
             },
         });
 
